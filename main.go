04a30e7cc7a043aa7e04074e06b8524de3adf3ef
Michael Berger <mberger@sipwise.com>
2020-11-25 21:51:11 +0100

    Correctly check 'Accept' field in headers
    Issue: #master

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
+// This is a very important package
+
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
 	"database/sql"
 
@@ -22,28 +26,37 @@ func main() {
 	fmt.Println(err)
 	db, err = sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/surveys")
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	initCatalogs()
 	ocai = generateOCAICatalog()
+
 	ocai.WriteJSON("data/ocai.json")
-	gin.SetMode(gin.ReleaseMode)
+
+	// gin.SetMode(gin.ReleaseMode)
+
 	router = gin.Default()
 	store := cookie.NewStore([]byte("secret"))
+
 	router.Use(sessions.Sessions("SESSIONID", store))
 	router.LoadHTMLGlob("templates/*")
+
 	initializeRoutes()
-	router.RunTLS(":443", "cert/fullchain.pem", "cert/privkey.pem")
+	log.Println("Starting router")
+	//router.RunTLS(":443", "cert/fullchain.pem", "cert/privkey.pem")
+	router.Run("localhost:8080")
 }
 
 func render(c *gin.Context, data gin.H, templateName string) {
 	data["catalogs"] = getAllCatalogs()
 	data["ocai"] = ocai
-
-	switch c.Request.Header.Get("Accept") {
-	case "application/json":
+	headers := c.Request.Header.Get("Accept")
+	log.Println(headers)
+	log.Println(c.Request.Header.Get("auth_token"))
+	switch {
+	case strings.Contains(headers, "application/json"):
 		c.JSON(http.StatusOK, data["payload"])
-	case "application/xml":
+	case headers == "application/xml":
 		c.XML(http.StatusOK, data["payload"])
 	default:
 		c.HTML(http.StatusOK, templateName, data)
