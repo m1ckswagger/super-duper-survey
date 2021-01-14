3522bbc65940ec48cd3483ae6b8db40f4f5fe547
Michael Berger <mberger@sipwise.com>
2021-01-14 13:54:47 +0100

    Add grpc gateway to third_party
    Issue: #grpc

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -2,5 +2,15 @@
 protoc --proto_path=api/proto/v1 --proto_path=third_party \
     --go_out=pkg/api/v1 --go_opt=paths=source_relative \
     --go-grpc_out=pkg/api/v1 --go-grpc_opt=paths=source_relative \
+    catalog.proto
+
+protoc --proto_path=api/proto/v1 --proto_path=third_party \
+    --go_out=pkg/api/v1 --go_opt=paths=source_relative \
+    --go-grpc_out=pkg/api/v1 --go-grpc_opt=paths=source_relative \
     question.proto
-    # catalog.proto
+
+protoc --proto_path=api/proto/v1 --proto_path=third_party \
+    --grpc-gateway_out=logtostderr=true:pkg/api/v1 catalog.proto
+
+protoc --proto_path=api/proto/v1 --proto_path=third_party \
+    --swagger_out=logtostderr=true:api/swagger/v1 catalog.proto
